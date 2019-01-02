73e86f62bbb9a7d3c9a0581620ff16cfc2cc966d
Josh <jlockrow@gmail.com>
2019-01-02 15:20:41 -0600

    chore: adds provided codebase for Student Class

---
+++ src/org/launchcode/java/school/Student.java
@@ -0,0 +1,30 @@
+package org.launchcode.java.school;
+
+public class Student {
+
+    private static int nextStudentId = 1;
+    private String name;
+    private final int studentId;
+    private int numberOfCredits;
+    private double gpa;
+
+    public Student(String name, int studentId,
+                   int numberOfCredits, double gpa) {
+        this.name = name;
+        this.studentId = studentId;
+        this.numberOfCredits = numberOfCredits;
+        this.gpa = gpa;
+    }
+
+    public Student(String name, int studentId) {
+        this(name, studentId, 0, 0);
+    }
+
+    public Student(String name) {
+        this(name, nextStudentId);
+        nextStudentId++;
+    }
+
+    /* getters and setters  */
+
+}
