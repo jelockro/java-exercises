ba24002c797eb9a349bf8bae6f38f8750afafcee
Josh <jlockrow@gmail.com>
2019-01-02 15:43:00 -0600

    chore: adds instance variables

---
+++ src/org/launchcode/java/school/Course.java
@@ -0,0 +1,17 @@
+package org.launchcode.java.school;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
+public class Course {
+    private String courseName;
+    private int courseID;
+    private ArrayList<String> parentDepartment;
+    private ArrayList<String> contentCategories;
+    private int maxEnrollment;
+    private int currentEnrollment;
+    private int minEnrollment;
+    private HashMap<Integer,  String> studentAndIds;
+
+}
